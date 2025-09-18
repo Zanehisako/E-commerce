2610466afbdf0f8448e60048d61d2528d8226c3b
Zanehisako <yassinetakiko@gmail.com>
2025-09-18 15:32:57 +0100

    Refactor ItemCard component to use centralized styles; improve readability and maintainability of code

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image"
 import Phone from "@/types/phone";
 
 export default function ItemCard({ phone }: { phone: Phone }) {
   return (
-    <View style={{ position: "relative", height: "100%" }}>
-      <View>
-        <Image style={{ borderRadius: 20, height: 400, width: 320 }} source={{ uri: phone.url }}></Image>
-        <Text style={{ fontSize: 16, fontWeight: "bold", alignSelf: "center" }}>{phone.name}</Text>
-        <Text style={{ fontSize: 14, fontWeight: "normal", alignSelf: "center" }}>${phone.price}</Text>
-      </View >
+    <View style={styles.itemCard}>
+      <Image style={styles.image} source={{ uri: phone.url }}></Image>
+      <Text style={styles.nameText}>{phone.name}</Text>
+      <Text style={styles.priceText}>${phone.price}</Text>
     </View >
   )
 }
+const styles = StyleSheet.create((
+  {
+    itemCard: {
+      position: "relative", height: "100%"
+    },
+    image: { backgroundColor: "#EFF1F3", borderRadius: 20, height: 400, width: 320 },
+    nameText: { fontSize: 16, fontWeight: "bold", alignSelf: "center" },
+    priceText: { fontSize: 14, fontWeight: "normal", alignSelf: "center" }
+  }
+))
