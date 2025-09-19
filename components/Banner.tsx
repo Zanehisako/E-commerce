2ff9f933e301a69cfb3fd28ee49f26a41c342d50
Zanehisako <yassinetakiko@gmail.com>
2025-09-19 12:12:56 +0100

    banner scroll animation is now working using setInterval

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,6 +7,18 @@ import { useEffect, useRef, useState } from "react";
 import supabase from "@/app/supabaseClient";
 import Animated from "react-native-reanimated";
 
+function useEffectAfterMount(effect: React.EffectCallback, deps?: React.DependencyList) {
+  const hasMounted = useRef(false);
+
+  useEffect(() => {
+    if (hasMounted.current) {
+      return effect();
+    } else {
+      hasMounted.current = true;
+    }
+  }, deps);
+}
+
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
@@ -16,27 +28,20 @@ const bannerWidth = 350
 export default function Banner() {
   const [banners, setbanners] = useState<string[]>([])
   const legendListRef = useRef<LegendListRef>(null)
-  const scrollIndex = useRef<number>(0)
+  const [scrollIndex, setScrollIndex] = useState<number>(0)
+  const imagesLoaded = useRef<boolean>(false)
 
 
   function animateScroll() {
     if (banners.length > 0) {
       if (legendListRef.current) {
         console.log("animating scroll")
-        console.log("scrollIndex:", scrollIndex.current)
-        legendListRef.current.scrollToIndex({ index: scrollIndex.current, animated: true })
-
-        setTimeout(() => {
-        }, 5000);
-
-        if (scrollIndex.current > banners.length) {
-          scrollIndex.current = 0
-        } else {
-          scrollIndex.current += 1;
-        }
+        console.log("scrollIndex:", scrollIndex)
+        legendListRef.current.scrollToIndex({ index: scrollIndex, animated: true })
       }
     }
   }
+
 
   async function getBanners() {
 
@@ -52,21 +57,27 @@ export default function Banner() {
       return object.name
     })
 
-    const fr = new FileReader;
     //console.log("names.length", names.length)
-    names.forEach(async (name) => {
+    const getBanners = names.map(async (name) => {
       const { data, error } = await supabase.storage.from('images').download(`banners/${name}`)
       if (error) {
         throw error
       }
-      if (data) {
-        fr.readAsDataURL(data)
-        fr.onload = () => {
-          setbanners((prev) => [...prev, fr.result as string])
+      return new Promise<string>((resolve) => {
+        const fr = new FileReader;
+        if (data) {
+          fr.onload = () => {
+            resolve(fr.result as string)
+          }
+          fr.readAsDataURL(data)
         }
-      }
+      })
     })
+    const results = await Promise.all(getBanners)
+    setbanners(results)
+    imagesLoaded.current = true
   }
+
 
   useEffect(() => {
     getBanners()
@@ -74,13 +85,25 @@ export default function Banner() {
 
   useEffect(() => {
     console.log("banners length:", banners.length)
-    animateScroll()
   }, [banners])
 
   useEffect(() => {
-    console.log("scroll index changed")
-    animateScroll()
-  }, [scrollIndex.current])
+    console.log("imagesLoaded")
+    const interval = setInterval(() => {
+      setScrollIndex(prev =>
+        prev >= banners.length - 1 ? 0 : prev + 1
+      )
+    }, 5000) // Change every 3 seconds
+    return () => clearInterval(interval) // Cleanup
+  }, [imagesLoaded.current === true])
+
+  useEffect(() => {
+    if (imagesLoaded.current === true) {
+      animateScroll()
+    }
+  }, [scrollIndex])
+
+
 
   return (
     <View style={{ height: 300, width: "100%" }}>
