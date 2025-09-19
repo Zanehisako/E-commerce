f4f7925e1ad89e73db1c6b76f3517df33277cc11
Zanehisako <yassinetakiko@gmail.com>
2025-09-19 11:04:29 +0100

    changed var name images to banners started working on banner scrolling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,36 +2,58 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image"
 import Phone from "@/types/phone";
 
-import { LegendList } from "@legendapp/list"
-import { useEffect, useState } from "react";
+import { LegendList, LegendListRef } from "@legendapp/list"
+import { useEffect, useRef, useState } from "react";
 import supabase from "@/app/supabaseClient";
+import Animated from "react-native-reanimated";
 
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
+
+const bannerWidth = 350
+
 export default function Banner() {
-  const [images, setImages] = useState<string[]>([])
+  const [banners, setbanners] = useState<string[]>([])
+  const legendListRef = useRef<LegendListRef>(null)
+  const scrollIndex = useRef<number>(0)
 
-  async function downloadImages(images: string[]) {
 
+  function animateScroll() {
+    if (banners.length > 0) {
+      if (legendListRef.current) {
+        console.log("animating scroll")
+        console.log("scrollIndex:", scrollIndex.current)
+        legendListRef.current.scrollToIndex({ index: scrollIndex.current, animated: true })
+
+        setTimeout(() => {
+        }, 5000);
+
+        if (scrollIndex.current > banners.length) {
+          scrollIndex.current = 0
+        } else {
+          scrollIndex.current += 1;
+        }
+      }
+    }
   }
 
-  async function getImages() {
+  async function getBanners() {
 
-    console.log('Downloading images',)
+    //console.log('Downloading banners',)
 
     const { data, error } = await supabase.storage.from('images').list("banners")
 
     if (error) { console.log("error:", error); throw error }
 
-    console.log("banners data:", data)
+    //console.log("banners data:", data)
 
     const names = data?.map((object) => {
       return object.name
     })
 
     const fr = new FileReader;
-    console.log("names.length", names.length)
+    //console.log("names.length", names.length)
     names.forEach(async (name) => {
       const { data, error } = await supabase.storage.from('images').download(`banners/${name}`)
       if (error) {
@@ -40,23 +62,32 @@ export default function Banner() {
       if (data) {
         fr.readAsDataURL(data)
         fr.onload = () => {
-          setImages((prev) => [...prev, fr.result as string])
+          setbanners((prev) => [...prev, fr.result as string])
         }
       }
     })
   }
 
   useEffect(() => {
-    getImages()
+    getBanners()
   }, [])
+
   useEffect(() => {
-    console.log("banners length:", images.length)
-  }, [images])
+    console.log("banners length:", banners.length)
+    animateScroll()
+  }, [banners])
+
+  useEffect(() => {
+    console.log("scroll index changed")
+    animateScroll()
+  }, [scrollIndex.current])
+
   return (
-    <ScrollView style={{ height: 300, width: "100%" }}>
-      {images.length > 0 && (
+    <View style={{ height: 300, width: "100%" }}>
+      {banners.length > 0 && (
         <LegendList
-          data={images}
+          ref={legendListRef}
+          data={banners}
           renderItem={({ item }) => <Image style={styles.image} source={{ uri: item }} contentFit="fill" cachePolicy={"memory-disk"} placeholder={{ blurhash }} />
           }
           keyExtractor={(_item, i) => `${i}`}
@@ -66,11 +97,11 @@ export default function Banner() {
         ></LegendList >
       )
       }
-      {/* {images.length > 0 && images.map((image, i) => { */}
+      {/* {banners.length > 0 && banners.map((image, i) => { */}
       {/*   return <Image id={`${image}${i}`} style={styles.image} source={{ uri: image }} contentFit="contain" cachePolicy={"memory-disk"} placeholder={{ blurhash }} /> */}
       {/* }) */}
       {/* } */}
-    </ScrollView>
+    </View>
   )
 }
 const styles = StyleSheet.create((
@@ -78,6 +109,6 @@ const styles = StyleSheet.create((
     itemCard: {
       position: "relative", height: "100%"
     },
-    image: { borderRadius: 20, height: 300, width: 350 },
+    image: { borderRadius: 20, height: 300, width: bannerWidth },
   }
 ))
