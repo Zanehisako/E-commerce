1dc3ce1584541e97032a6ef6c5418e6fc7c259a5
Zanehisako <yassinetakiko@gmail.com>
2025-10-18 15:26:07 +0100

    Add ItemPage component with detailed item display and data fetching logic

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,17 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Image } from "expo-image"
 import Phone from "@/types/phone";
+import { router } from 'expo-router';
 
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function ItemCard({ phone }: { phone: Phone }) {
   return (
-    <View style={styles.itemCard}>
-      <Image style={styles.image} source={{ uri: phone.url }} cachePolicy={"memory-disk"} placeholder={{ blurhash }} contentFit="cover" transition={1000}></Image>
-      <Text style={styles.nameText}>{phone.name}</Text>
-      <Text style={styles.priceText}>${phone.price}</Text>
-    </View >
+    <TouchableOpacity onPress={() => router.push({
+      pathname: "/itemPage",
+      params: {
+        id: phone.id
+      }
+    })}>
+      <View style={styles.itemCard}>
+        <Image style={styles.image} source={{ uri: phone.url }} cachePolicy={"memory-disk"} placeholder={{ blurhash }} contentFit="cover" transition={1000}></Image>
+        <Text style={styles.nameText}>{phone.name}</Text>
+        <Text style={styles.priceText}>${phone.price}</Text>
+      </View >
+    </TouchableOpacity>
   )
 }
 const styles = StyleSheet.create((
